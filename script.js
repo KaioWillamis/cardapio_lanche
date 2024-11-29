cc68e89ba93802ae05ba2b98418015515af179e2
KaioWillamis <brm560548@gmail.com>
2024-11-29 16:55:58 -0300

    atualizei

---
+++ script.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector(".menu");
+const cartBtn = document.querySelector("#cart-btn");
+const cartModal = document.querySelector(".modal");
+const cartItemsContainer = document.querySelector("#cart-items");
+const cartTotal = document.querySelector("#cart-total");
+const checkoutBtn = document.querySelector("#checkout-btn");
+const closeModalBtn = document.querySelector("#fechar_modal");
+const cartCounter = document.querySelector("#cart-count");
+const nome = document.querySelector("#nome");
+const nomeErro = document.querySelector("#nome_cliente");
+
+cartBtn.addEventListener("click", function(){
+    cartModal.style.display = "flex";
+})
